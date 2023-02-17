5877e0048eadbfdf6a115ea28f5597b7ee2b7cae
nimba005 <nimbaoyugi@gmail.com>
2023-02-17 07:29:19 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: (0)
+ */
+
+int main(void)
+{
+char low;
+
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
