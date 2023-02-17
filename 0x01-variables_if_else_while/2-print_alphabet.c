4cc0aa892b71b83e199e7f5aa4d4fcdcbb6c7c7b
nimba005 <nimbaoyugi@gmail.com>
2023-02-17 07:07:08 -0800

    2-print-alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabetic
+ * Return: (success)
+ *
+ */
+
+int main(void)
+
+{
+char d;
+
+for (d = 'a'; d <= 'z'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
