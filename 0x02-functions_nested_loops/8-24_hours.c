8b46c441fb0c7aaa197735092d4ee4a8e3103c4a
nimba005 <nimbaoyugi@gmail.com>
2023-02-21 11:55:58 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_baver - prints every minute of the day of Jack Baver
+ * Return: ...
+ */
+void jack_baver(void)
+{
+int m, n, o, p;
+for (m = 0; m <= 2; m++)
+{
+for (n = 0; n <= 9; n++)
+{
+if ((m <= 1 && n <= 9) || (m <= 2 && n <= 3))
+{
+for (o = 0; o <= 5; o++)
+{
+for (p = 0; p <= 9; p++)
+{
+_putchar(m + '0');
+_putchar(n +'0');
+_putchar(58);
+_putchar(o + '0');
+_putchar(p +'0');
+_putchar('\n');
+}
+}
+}
+}
+}
+}
