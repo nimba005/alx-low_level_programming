25e96ebbf40285a6921d9d15a94d57872f2dd812
nimba005 <nimbaoyugi@gmail.com>
2023-02-23 06:18:50 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,17 @@
 
 /**
  * print_numbers - print numbers from 0 to 9
- * Return: succes
+ * Return: success
  */
 
 void print_numbers(void)
 
 {
-	int c;
+int x;
 
-	for (c = 0; c <= 9; c++)
-	{
-	_putchar(c + '0')
-	}
-	_putchar('\n');
+for (x = 0; x <= 9; x++)
+{
+_putchar(x + '0');
+}
+_putchar('\n');
 }
