b1252babc9579fca30d532cdc59d28b4d08a34b8
nimba005 <nimbaoyugi@gmail.com>
2023-03-13 23:14:16 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,23 +1,47 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * strtow - splits a string into words
- * @str: a sring to be counted
- * return: null
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ * Return: number of words
+ */
+
+int count_word(char *s)
+{
+int flag, c, w;
+flag = 0;
+w = 0;
+for (c = 0; s[c] != '\0'; c++)
+{
+if (s[c] == ' ')
+flag = 0;
+else if (flag == 0)
+{
+flag = 1;
+w++;
+}
+}
+return (w);
+}
+
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 
 char **strtow(char *str)
 {
 char **matrix, *tmp;
-int i, k = 0, len = 0, word, c = 0, start, end;
+int i, k = 0, len = 0, words, c = 0, start, end;
 while (*(str + len))
 len++;
-words (str);
-if (word == 0)
+words = count_word(str);
+if (words == 0)
 return (NULL);
-matrix = (char **) malloc(sizeof(char *) * (word + 1));
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
 if (matrix == NULL)
 return (NULL);
 for (i = 0; i <= len; i++)
