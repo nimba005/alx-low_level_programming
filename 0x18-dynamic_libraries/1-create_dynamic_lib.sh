b2e5ff47fcec86bb34d37731a281f42053213b96
nimba005 <nimbaoyugi@gmail.com>
2023-08-21 19:20:31 +0000

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c
-gcc -shared -0 liball.so *.o
-rm *.o
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
